736dc3a9dd991f8a484be3d1f1c463eede598998
yiyanggis <yiyanggis@gmail.com>
2024-08-27 22:27:40 -0400

    Update layer url
    try url with secure domain

--- js/layersList.js
+++ js/layersList.js
@@ -255,7 +255,7 @@ const layers = [
     source: {
       type: "raster",
       tiles: [
-        "https://66.175.239.115/geoserver/HSR_Raster/wms?service=WMS&version=1.1.0&request=GetMap&layers=mappingNY%3AHSR&bbox={bbox-epsg-3857}&width=768&height=481&srs=EPSG%3A3857&styles=&format=image/png&transparent=true",
+        "https://dreamy-darwin.66-175-239-115.plesk.page/geoserver/HSR_Raster/wms?service=WMS&version=1.1.0&request=GetMap&layers=mappingNY%3AHSR&bbox={bbox-epsg-3857}&width=768&height=481&srs=EPSG%3A3857&styles=&format=image/png&transparent=true",
       ],
       tileSize: 256,
     },
